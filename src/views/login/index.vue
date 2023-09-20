1087f0e6e22ec5a39bf33201be60d307dbc7c00a
theadmaster <55393861+Theadmaster@users.noreply.github.com>
2023-09-20 15:23:57 +0800

    修复了一些已知问题

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -75,7 +75,7 @@ export default {
     return {
       loginForm: {
         username: 'admin',
-        password: 'tytyt.000'
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
